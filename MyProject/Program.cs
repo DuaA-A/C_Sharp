d1b66701740e8372733350d4e7a11be850d64850
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2025-01-05 17:53:23 +0200

    first prject in C#

---
+++ MyProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace challengeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            bool flag = true;
+            int value = 0;
+            if (flag)
+                Console.WriteLine($"Inside the code block: {value}");
+            value = 10;
+            Console.WriteLine($"Outside the code block: {value}");
+        }
+    }
+}
