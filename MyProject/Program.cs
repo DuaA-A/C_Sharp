ae039b3fa52faec84bd7c7d25ca26ede55d257ff
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2025-01-05 18:09:45 +0200

    Variable scope challenge

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -7,12 +7,25 @@ namespace challengeProject
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
-            bool flag = true;
-            int value = 0;
-            if (flag)
-                Console.WriteLine($"Inside the code block: {value}");
-            value = 10;
-            Console.WriteLine($"Outside the code block: {value}");
+            // bool flag = true;
+            // int value = 0;
+            // if (flag)
+            //     Console.WriteLine($"Inside the code block: {value}");
+            // value = 10;
+            // Console.WriteLine($"Outside the code block: {value}");
+            //------------------------------
+            //Variable scope challenge
+            int[] numbers = { 4, 8, 15, 16, 23, 42 };
+                int total=0;
+                bool found=false;
+            foreach (int number in numbers){
+                total += number;
+                if (number == 42)
+                    found = true;
+            }
+            if (found) 
+                Console.WriteLine("Set contains 42");
+            Console.WriteLine($"Total: {total}");
         }
     }
 }
--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1b66701740e8372733350d4e7a11be850d64850")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
